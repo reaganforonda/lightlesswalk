2380e6212fd3883386c7a82dc492260faf98412c
reaganforonda <foronda.reagan@gmail.com>
2019-08-03 16:30:44 -0600

    login form

---
+++ src/Components/Landing/LoginForm.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+
+export default class LoginForm extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ""
+        }
+    }
+
+    handleInputChange(e) {
+        this.setState({[e.target.name] : e.target.value});
+    }
+
+    render() {
+        return (
+            <form>
+                <div>
+                    <input name='email' type='email' placeholder='Email' 
+                        onChange={(e)=>this.handleInputChange(e)}/>
+                </div>
+                <div>
+                    <input name='password' type='password' placeholder='Password' 
+                        onChange={(e)=>this.handleInputChange(e)} />
+                </div>
+                <div>
+                    <button>Log In</button>
+                </div>
+            </form>
+        )
+    }
+}
