08f587eb20f909bd1644d950350c5fa2cd35b943
reaganforonda <foronda.reagan@gmail.com>
2019-08-03 16:08:14 -0600

    add landing route

--- src/routes.js
+++ src/routes.js
@@ -2,3 +2,9 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Landing from './Components/Landing/Landing';
 
+export default (
+    <Switch>
+        <Route exact path='/' component={Landing}/>
+    </Switch>
+)
+
