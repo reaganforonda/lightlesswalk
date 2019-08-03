0c9e75474f79ccbc5488a54eaf1229ecb4c4ebf7
reaganforonda <foronda.reagan@gmail.com>
2019-08-03 17:03:19 -0600

    user store

--- src/store.js
+++ src/store.js
@@ -1,9 +1,9 @@
 import {createStore, applyMiddleware, combineReducers} from 'redux';
 import promiseMiddleware from 'redux-promise-middleware';
-
+import userReducer from './ducks/reducers/userReducer';
 
 const reducers = {
-
+    userReducer: userReducer
 }
 
 let middleware = promiseMiddleware
