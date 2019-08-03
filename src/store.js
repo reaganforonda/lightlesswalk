6882c2e0e5153e4f1f8673b73c98850561ba0edf
reaganforonda <foronda.reagan@gmail.com>
2019-08-03 16:11:42 -0600

    store

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+import promiseMiddleware from 'redux-promise-middleware';
+
+
+const reducers = {
+
+}
+
+let middleware = promiseMiddleware();
+
+export default createStore(combineReducers(reducers), applyMiddleware(middleware));
