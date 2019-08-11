9110703dd767d6520ad150f6a36e6d94c1c0e38b
reaganforonda <foronda.reagan@gmail.com>
2019-08-11 07:51:54 -0600

    main page route

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Landing from './Components/Landing/Landing';
+import MainPage from './Components/Main/MainPage';
 
 export default (
     <Switch>
         <Route exact path='/' component={Landing}/>
+        <Route path='/main' component={MainPage}/>
     </Switch>
 )
 
