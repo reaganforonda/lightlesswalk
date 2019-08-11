78707917e4ab83dcd6160ecf600f4645d0c7034c
reaganforonda <foronda.reagan@gmail.com>
2019-08-11 07:51:44 -0600

    main page

---
+++ src/Components/Main/MainPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class MainPage extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return (
+            <div>
+                Main Page
+            </div>
+        )
+    }
+}
